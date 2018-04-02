c920131877cca58e1c51325ec3a31230994eda26
kirineko <kirinekoneko@gmail.com>
2018-04-03 03:13:20 +0800

    添加微信配置

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hello, this is a test');
+  try {
+    let data = req.params;
+    if (Object.keys(data).length == 0) {
+      res.send('no params');
+    }
+    let signature = data.signature;
+    let timestamp = data.timestamp;
+    let nonce = data.nonce;
+    let echostr = data.echostr;
+    let token = 'hello2016';
+    
+    let list = [token, timestamp, nonce];
+    list.sort();
+    let sha1 = crypto.createHash('sha1');
+    list.map(sha1.update());
+    let hashcode = sha1.digest('hex');
+    console.log("handle/GET func: hashcode, signature: ", hashcode, signature);
+    if (hashcode == signature) {
+      res.send(echostr)
+    } else {
+      res.send('')
+    }
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
